73d09eec3cde59945a174e579f5bc5c944abd4c5
Rafael-P <r4f4el_porto@hotmail.com>
2020-11-19 16:22:35 -0300

    Arrays

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Arrays_19_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercicio 19-11");
+
+            string[] nomes = new string[5];
+            int[] idades = new int[5];
+
+            for (var contador = 0; contador < 5; contador++)
+            {
+                Console.WriteLine($"Digite o {contador+1}º nome");
+                nomes[contador] = Console.ReadLine();
+                Console.WriteLine("Digite a idade");
+                idades[contador] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Nomes Cadastrados:");
+
+            for (var contador = 0; contador < 5; contador++)
+            {
+                Console.WriteLine("Nome: "+nomes[contador]);
+                Console.WriteLine("Idade: "+idades[contador]);
+            }
+
+        }//FIM DO MAIN
+    }
+}
